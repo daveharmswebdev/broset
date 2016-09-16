96a9a575098fd2a3dbec7c15293c6adef39fcfa5
Dave Harms <daveharmswebdev@gmail.com>
2016-09-16 15:46:03 -0500

    served up status files, using pug

--- src/server.js
+++ src/server.js
@@ -1,15 +1,22 @@
 'use strict'
 
 const express = require('express')
+const bodyParser = require('body-parser')
 const app = express()
 
 const port = process.env.PORT || 3000
 app.set('port', port)
 
+// using pug to server static files
+app.set('views', 'views')
+app.set('view engine', 'pug')
+app.use(express.static('public'))
+app.use(bodyParser.urlencoded({ extended: false }));
+
 app.get('/', (req, res) => {
-	res.send('hello world!')
+	res.render('home.pug')
 })
 
 app.listen(app.get('port'), () => {
-	console.log(`Express running and now lisening on ${app.get('port')}`)
+	console.log(`Express running and now listening on ${app.get('port')}`)
 })
