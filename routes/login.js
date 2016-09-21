efd76a6abd40f2395bf6ffc1ab8818400e550196
Dave Harms <daveharmswebdev@gmail.com>
2016-09-21 11:48:31 -0500

    made controllers for login and register and connected them to login and register routes

--- routes/login.js
+++ routes/login.js
@@ -2,41 +2,10 @@
 
 const { Router } = require('express')
 const router = Router()
+const ctrl = require('../controllers/login')
 
-const User = require('../models/user')
-const bcrypt = require('bcrypt')
+router.post('/login', ctrl.show)
 
-router.post('/login', ({ session, body: {user, pass}}, res, next) => {
-	User
-		.findOne({user})
-		.then( user => {
-			if (user) {
-				return new Promise((resolve, reject) => {
-					bcrypt.compare(pass, user.pass, (err, matches) => {
-						if (err) {
-							reject(err)
-						} else {
-							resolve(matches)
-						}
-					})
-				})
-			} else {
-				res.render('login', { error: 'That email is not registered with a user' })
-			}
-		})
-		.then( matches => {
-			if (matches) {
-				session.user = user
-				res.redirect('/')
-			} else {
-				res.render('login', {error: 'password does not match!'})
-			}
-		})
-		.catch( err => next(err))
-})
-
-router.get('/login', (req, res) => {
-	res.render('login', {})
-})
+router.get('/login', ctrl.index)
 
 module.exports = router
--- routes/register.js
+++ routes/register.js
@@ -2,38 +2,10 @@
 
 const { Router } = require('express')
 const router = Router()
+const ctrl = require('../controllers/register')
 
-const User = require('../models/user')
-const bcrypt = require('bcrypt')
+router.get('/register', ctrl.new)
 
-router.get('/register', (req, res) => {
-	res.render('register')
-})
-
-router.post('/register', ({body: {user, pass, confirmation}}, res) => {
-	if (pass === confirmation) {
-		User
-			.findOne({user})
-			.then( user => {
-				if (user) {
-					res.render('register', {error: 'email already being used'})
-				} else {
-					return new Promise((resolve, reject) => {
-						bcrypt.hash(pass, 15, (err,hash) => {
-							if (err) {
-								reject(err)
-							} else {
-								resolve(hash)
-							}
-						})
-					})
-				}
-			})
-			.then(hash => User.create({ user, pass: hash}))
-			.then(() => res.redirect('/'))
-	} else {
-		res.render('register', { error: 'Password and confirm do not match'})
-	}
-})
+router.post('/register', ctrl.create)
 
 module.exports = router
